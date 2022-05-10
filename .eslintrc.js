694b1fdf27e0ddacf0939348256c5fd3a16ce38e
Alexey Degtyarev <a.degtyarev@ideco.ru>
2022-05-11 00:07:43 +0500

    create main layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,65 @@
 module.exports = {
   extends: [
-    "react-app", // см. пункт 1
-    "plugin:prettier/recommended", // см. пункт 2
+    'react-app',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["jsdoc"],
+  plugins: ['jsdoc'],
   rules: {
-    curly: "error",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "jsx-a11y/alt-text": "off",
-    "jsx-a11y/anchor-has-content": "off",
-    "jsx-a11y/anchor-is-valid": "off",
-    "import/order": [
-      "error",
+    curly: 'error',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'jsx-a11y/anchor-has-content': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'import/order': [
+      'error',
       {
-        "newlines-between": "never",
+        'newlines-between': 'never',
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: false,
         },
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type",
-        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
         pathGroups: [
           {
-            pattern: "react",
-            group: "builtin",
-            position: "before",
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
           },
         ],
-        pathGroupsExcludedImportTypes: ["react"],
+        pathGroupsExcludedImportTypes: ['react'],
       },
     ],
-    "import/no-anonymous-default-export": "off",
-    "import/newline-after-import": ["error"],
+    'import/no-anonymous-default-export': 'off',
+    'import/newline-after-import': ['error'],
 
-    "sort-imports": [
-      "error",
+    'sort-imports': [
+      'error',
       {
         ignoreCase: false,
         ignoreDeclarationSort: true,
         ignoreMemberSort: false,
-        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
         allowSeparatedGroups: false,
       },
     ],
 
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { vars: "all", args: "after-used", argsIgnorePattern: "^_" },
+    'jsdoc/require-param-name': ['error'],
+    'jsdoc/no-types': ['error'],
+    'jsdoc/empty-tags': ['error'],
+    'jsdoc/check-tag-names': ['error'],
+    'jsdoc/require-description-complete-sentence': ['error'],
+    'cypress/no-unnecessary-waiting': 'off',
+
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', argsIgnorePattern: '^_' },
     ],
 
-    "no-undef": "error",
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    'no-undef': 'error',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
   },
   globals: {
     React: true,
